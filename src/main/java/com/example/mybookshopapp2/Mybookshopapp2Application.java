48bb77b5007b3e87b49e7a4d2e7ae6746e5b14dd
matua <$9x=4cg^zNNbF&H.UQR_vmN8jSb2U95>
2021-03-22 20:06:25 +0300

    init setup

---
+++ src/main/java/com/example/mybookshopapp2/Mybookshopapp2Application.java
@@ -0,0 +1,13 @@
+package com.example.mybookshopapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybookshopapp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybookshopapp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybookshopapp2/Mybookshopapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybookshopapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybookshopapp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
