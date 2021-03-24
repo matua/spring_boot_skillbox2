8345aa3378dc431ad5522431c707905ffa982e4b
matua <$9x=4cg^zNNbF&H.UQR_vmN8jSb2U95>
2021-03-24 20:21:13 +0300

    Подключить разделы интернет-магазина. 🦾
    Теперь в вашем распоряжении есть полная верстка всего интернет-магазина! Добавьте необходимую логику на backend-е и подключите разделы “Новинки”, “Популярное”, “Отложенное”, “Корзина”, “Войти”, “Поиск”, “Документы”, “О компании”, ”Помощь”, “Контакты”, а также обеспечьте возможность перехода между ними.

---
+++ src/main/java/com/example/mybookshopapp2/controllers/PostponedController.java
@@ -0,0 +1,34 @@
+package com.example.mybookshopapp2.controllers;
+
+import com.example.mybookshopapp2.data.Author;
+import com.example.mybookshopapp2.data.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class PostponedController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public PostponedController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @ModelAttribute("authorsMap")
+    public Map<String, List<Author>> authorsMap() {
+        Map<String, List<Author>> authorsMap = authorService.getAuthorsMap();
+        return authorService.getAuthorsMap();
+    }
+
+    @GetMapping("/postponed")
+    public String authorsPage() {
+        return "/postponed";
+    }
+
+}
---
+++ src/main/java/com/example/mybookshopapp2/controllers/RecentController.java
@@ -0,0 +1,34 @@
+package com.example.mybookshopapp2.controllers;
+
+import com.example.mybookshopapp2.data.Author;
+import com.example.mybookshopapp2.data.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class RecentController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public RecentController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @ModelAttribute("authorsMap")
+    public Map<String, List<Author>> authorsMap() {
+        Map<String, List<Author>> authorsMap = authorService.getAuthorsMap();
+        return authorService.getAuthorsMap();
+    }
+
+    @GetMapping("/recent")
+    public String authorsPage() {
+        return "/books/recent";
+    }
+
+}
